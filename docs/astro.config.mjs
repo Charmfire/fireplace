74eb35346698bf8b7bea64e50a216f7b043d9724
leswell <simofilevente+git@gmail.com>
2026-01-12 18:43:07 +0200

    fix(docs): updated site and base

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -4,6 +4,8 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+	site: "https://charmfire.github.io",
+  	base: "/fireplace",
 	integrations: [
 		starlight({
 			title: 'Fireplace Docs',
