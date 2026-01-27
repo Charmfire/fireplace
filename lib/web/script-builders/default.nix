c0214424d52754112eb9001e135a19bf50a9b277
leswell <simofilevente+git@gmail.com>
2026-01-28 01:21:00 +0200

    feat(web-scripts-builder): attribute-loader moved under plumbing

--- lib/web/script-builders/default.nix
+++ lib/web/script-builders/default.nix
@@ -1,29 +1,39 @@
-let 
-  kernel = import ../../kernel {};
-  scriptsBuilder = server-cmd: { pkgs }: reserved-name:
-  {
-    qr.exec = "qrencode -t ANSIUTF8 'https://${reserved-name}.share.zrok.io'";
-    tunnel.exec = ''
-      qr
-      echo 'https://${reserved-name}.share.zrok.io'
-      zrok share reserved ${reserved-name} --headless
+let
+  kernel = import ../../kernel { };
+  scriptsBuilder =
+    server-cmd:
+    { pkgs }:
+    reserved-name: {
+      qr.exec = "qrencode -t ANSIUTF8 'https://${reserved-name}.share.zrok.io'";
+      tunnel.exec = ''
+        qr
+        echo 'https://${reserved-name}.share.zrok.io'
+        zrok share reserved ${reserved-name} --headless
       '';
 
-    # HELPERS
-    _get-ip.exec = ''
-      #!${pkgs.bash}
-      ip -4 addr show wlo1 | grep -oP '(?<=inet\s)\d+(\.\d+){3}'
-    '';
-    
+      # HELPERS
+      _get-ip.exec = ''
+        #!${pkgs.bash}
+        ip -4 addr show wlo1 | grep -oP '(?<=inet\s)\d+(\.\d+){3}'
+      '';
 
-    server.exec = server-cmd;
-    dev.exec = (kernel.script-parts.parallel-run "server" "tunnel");
-  };
+      server.exec = server-cmd;
+      dev.exec = (kernel.script-parts.parallel-run "server" "tunnel");
+    };
+
+  attribute-loader = import ./plumbing/attribute-loader.nix;
 in
 {
-  attribute-loader = import ./attribute-loader.nix;
+  inherit attribute-loader;
   starter = import ./starter.nix;
-  scriptBuilders.astro =  import ./astro.nix {inherit scriptsBuilder;};
-  zolaScriptsBuilder =   import ./zola.nix {inherit scriptsBuilder;};
-  scriptBuilders.nextjs =   import ./nextjs.nix {inherit scriptsBuilder;};
+  zolaScriptsBuilder = import ./zola.nix { inherit scriptsBuilder; };
+
+  scriptBuilders = rec {
+    astro = import ./astro.nix { inherit scriptsBuilder; };
+    nextjs = import ./nextjs.nix { inherit scriptsBuilder; };
+
+    withEnv = {
+      astro = config: astro (attribute-loader config);
+    };
+  };
 }
